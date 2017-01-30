44e77026d3795d0a85085c3b07eb358aaf3697be
John Whiles <johnwhiles@outlook.com>
2017-01-30 16:40:54 +0000

    remove unused vars

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Cat = () => {
   return (
-    <img className="cat" src="./cat.png" />
+    <img alt="friendly cat" className="cat" src="./cat.png" />
   );
 }
 
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ResponseOption from './ResponseOption.js';
 
 const Message = (props) => {
