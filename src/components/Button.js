55d9b25ad45eda56b14292fc7bf83f753efb5762
John Whiles <johnwhiles@outlook.com>
2017-01-31 13:56:49 +0000

    add functionality to buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+const obj = {
+  stroke: (e) => console.log('stroke'),
+  talk: (e) => console.log('talk'),
+  feed: (e) => console.log('feed'),
+  play: (e) => console.log('play')
+}
+
 const Button = (props) => {
   return (
-    <button className="button">{props.label}</button>
+    <button onClick={(e) => obj[props.label](e)} className="button">{props.label}</button>
   );
 }
 
