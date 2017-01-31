1a8db4c742745011259779a108082c95644a47b6
Tom Barrett <tbtommyb@gmail.com>
2017-01-31 11:36:11 +0000

    Added ability to have conversation by choosing options

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import ResponseOption from './ResponseOption.js';
 
-const Message = (props) => {
-  return (
-    <div className="message">
-      <p>{props.text}</p>
-      {props.options.map(option => <ResponseOption text={option} />)}
-    </div>
-  );
+const convo = {
+  1: {
+    text: 'Hi! how are you feeling',
+    responses: [
+      { text: 'I feel sad', next: 2 },
+      { text: 'I am happy!', next: 3}
+    ]
+  },
+  2: {
+    text: 'Why are you sad',
+    responses: [
+      { text: 'I don\'t know', next: 3}
+    ]
+  },
+  3: {
+    text: 'Great, me too!',
+    responses: [
+      { text: 'I don\'t know', next: 3}
+    ]
+  }
+};
+
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      conversation: convo[1]
+    };
+    this.updateMessage = this.updateMessage.bind(this);
+  }
+  updateMessage(next) {
+    this.setState({ conversation: convo[next] });
+  }
+  render() {
+    const { conversation } = this.state;
+    return (
+      <div className="message">
+        <p>{conversation.text}</p>
+        {conversation.responses.map(response =>
+          <ResponseOption updateMessage={this.updateMessage}
+                          response={response} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default Message;
