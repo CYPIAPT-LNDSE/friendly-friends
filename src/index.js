8a79ba2f56aa5f23116d8c1389dd7f934f2dcd7c
John Whiles <johnwhiles@outlook.com>
2017-02-01 10:36:38 +0000

    introduce redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import reducers from './reducers';
 import App from './components/App';
 import './styles/index.css';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
