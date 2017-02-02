ee824b72b75a509109174a432ba8aae2db6489aa
Tom Barrett <tbtommyb@gmail.com>
2017-02-02 15:46:25 +0000

    Abstracted out modals, changed some class names and tweaked appearance of some elements

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 class WelcomeScreen extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {value: ''};
+      this.state = {value: ""};
       this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -14,15 +14,15 @@ class WelcomeScreen extends React.Component {
 
     render() {
     return (
-      <div className="welcomeScreen">
-        <img alt="Welcome cat" src="./cat.png" className="welcomeImage"/>
-        <p className="welcomeText">{"Hi! Great to meet you. I'm Sammy. What's your name?"}</p>
-        <form className="welcomeForm" onSubmit={this.handleSubmit}>
+      <div className="modal welcome">
+        <img alt="Welcome cat" src="./cat.png" className="modal--image"/>
+        <p className="welcome--text">{"Hi! Great to meet you. I'm Sammy. What's your name?"}</p>
+        <form className="welcome--form" onSubmit={this.handleSubmit}>
           <input
             type="text"
             placeholder="Enter your name"
             name="name"
-            className="welcomeInput"
+            className="form--input"
             ref={(input) => this.input = input} />
           <button type="submit" label="Submit">Submit</button>
         </form>
